766b13b77a97bf9dcc898df2edf17c90e80cfdf2
unknown <bastian1926@e-camp.cl>
2024-11-03 12:08:18 -0300

    Bootstrap para el proyecto, navbar y redireccionamiento

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,16 @@ INSTALLED_APPS = [
 
     # Aplicaciones desarrolladas para este proyecto
     'vehiculo',
+
+    # Registrar dependencias bootstrap 5
+    'django_bootstrap5',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+# Configurar crispy forms con bootstrap 5
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
